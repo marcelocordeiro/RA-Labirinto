2b28793191bbe52ab03a4cbe0db016a16418420e
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2015-11-20 12:03:40 -0200

    Adicionado esfera e movimento ao cubo

---
+++ Trabalho RA/Assets/Scripts/MoverCubo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverCubo : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float movX = Input.GetAxisRaw("Horizontal");
+        float movY = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(Vector3.left * (float) 0.03 * movX * Time.deltaTime);
+        transform.Translate(Vector3.down * (float) 0.03 * movY * Time.deltaTime);
+	}
+}
