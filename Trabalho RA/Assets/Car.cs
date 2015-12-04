b000992b132ffec26d13a114a6f5193f8d95d7d5
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2015-12-04 21:51:52 -0200

    Começando labirinto

---
+++ Trabalho RA/Assets/Car.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+    public GameObject carro;
+
+	// Use this for initialization
+	void Start () {
+        carro.SetActive(false);
+        carro.GetComponent<Renderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
