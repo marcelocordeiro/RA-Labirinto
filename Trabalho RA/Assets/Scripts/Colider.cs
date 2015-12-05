321c37f12df6430a93cfce62be8c24866f2389fe
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2015-12-04 22:16:32 -0200

    Adicionado esfera

---
+++ Trabalho RA/Assets/Scripts/Colider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Colider : MonoBehaviour
+{
+
+    void OnCollisionEnter (Collision col)
+    {
+        Destroy(this.gameObject);
+    }
+
+}
+
