9b35d58d4f1e78b4648f6d8b04ca14fe1336be1c
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2015-12-09 12:56:28 -0200

    Funcionando com todas as cenas

--- Trabalho RA/Assets/Scripts/Colider.cs
+++ Trabalho RA/Assets/Scripts/Colider.cs
@@ -1,13 +1,42 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
 public class Colider : MonoBehaviour
 {
+    int pontos = 100;
+
+    Color old = RenderSettings.ambientLight;
+
+    public GameObject esfera;
+    public GameObject finish;
 
     void OnCollisionEnter (Collision col)
     {
-        Destroy(this.gameObject);
+        Color cor = Color.red;
+        if (col.gameObject == finish)
+        {
+            cor = Color.green;
+            old = Color.green;
+        }
+        RenderSettings.ambientLight = cor;
     }
 
+    void OnCollisionExit (Collision col)
+    {
+        RenderSettings.ambientLight = old;
+        if (old == Color.green)
+        {
+            EditorUtility.DisplayDialog("Fim de jogo", "Sua pontuação final é " + pontos, "Ok");
+            pontos = 100;
+        }
+        else
+            pontos--;
+    }
+
+    void Update()
+    {
+        //esfera.transform.position = new Vector3(this.transform.position.x, plano.transform.position.y, this.transform.position.z);
+    }
 }
 
