616d781a9ddf55aaca92275322c5165172333432
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2015-12-11 16:08:02 -0200

    Versão final mesmo

--- Trabalho RA/Assets/Scripts/Colider.cs
+++ Trabalho RA/Assets/Scripts/Colider.cs
@@ -1,21 +1,39 @@
 ﻿using UnityEngine;
-using UnityEditor;
 using System.Collections;
 
 public class Colider : MonoBehaviour
 {
-    int pontos = 100;
+    int pontos;
 
-    Color old = RenderSettings.ambientLight;
+    Color old;
 
     public GameObject esfera;
     public GameObject finish;
+
+
+    private Rect windowRect = new Rect((Screen.width - 200) / 2, (Screen.height - 100) / 2, 200, 80);
+    private bool show = false;
+
+    void Start ()
+    {
+        old = RenderSettings.ambientLight;
+        pontos = 100;
+    }
+
+    void Update ()
+    {
+        if (old == Color.green)
+        {
+            Open();
+        }
+    }
 
     void OnCollisionEnter (Collision col)
     {
         Color cor = Color.red;
         if (col.gameObject == finish)
         {
+            RenderSettings.ambientLight = Color.green;
             cor = Color.green;
             old = Color.green;
         }
@@ -25,13 +43,31 @@ public class Colider : MonoBehaviour
     void OnCollisionExit (Collision col)
     {
         RenderSettings.ambientLight = old;
-        if (old == Color.green)
-        {
-            EditorUtility.DisplayDialog("Fim de jogo", "Sua pontuação final é " + pontos, "Ok");
-            Application.Quit();
-        }
-        else
+        if (old != Color.green)
             pontos--;
+    }
+
+    void OnGUI()
+    {
+        if (show)
+            windowRect = GUI.Window(0, windowRect, DialogWindow, "Parabéns!");
+    }
+
+    void DialogWindow(int windowID)
+    {
+        float y = 20;
+        GUI.Label(new Rect(5, y, windowRect.width, 20), "Sua pontuação final é " + pontos);
+
+        if (GUI.Button(new Rect(5, y + 35, windowRect.width - 10, 20), "Ok"))
+        {
+            Application.Quit();
+            show = false;
+        }
+    }
+
+    void Open()
+    {
+        show = true;
     }
 }
 
